e9b6571d458736c37ac12552142295712f03d278
TIFF DURR <TFFNDRR@GMAIL.COM>
2023-04-03 02:34:29 +0200

    style

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const scoreDIV = document.getElementById("score");
 const viesDIV = document.getElementById("vies");
 const reponseDiv = document.getElementById("titreChanson");
 const trouveesDiv = document.getElementById("chansonstrouvees");
+const iVolumeDown = document.querySelector(".fa-volume-down");
+const iVolumeUp = document.querySelector(".fa-volume-up");
+const form = document.querySelector("form");
+const bonus = document.getElementById("bonus");
 
 const input = document.querySelector("input");
 const volMoins = document.getElementById("moins");
 const volPlus = document.getElementById("plus");
 const volumeDiv = document.getElementById("volume");
+const volumeTitre = document.querySelector(".volumeTitre");
 const volumeText = document.getElementById("volText");
+const lectureDIV = document.querySelector(".lecture");
 let v = 0.5;
 let w = 5;
 
@@ -43,11 +49,17 @@ function init() {
   boutonValider.style.display = "none";
   boutonRejouer.style.display = "none";
   input.style.display = "none";
+  form.style.display = "none";
+  timerContainer.style.display = "none";
   viesDIV.innerHTML = `<h1><i class="fas fa-heart"></i><i class="fas fa-heart"></i><i class="fas fa-heart"></i></h1>`;
+  viesDIV.style.display = "none";
   boutonNouvellePartie.style.display = "none";
   boutonReponse.style.display = "none";
   boutonSuivant.style.display = "none";
   volumeDiv.style.display = "none";
+  volumeTitre.style.display = "none";
+  scoreDIV.style.display = "none";
+  lectureDIV.style.display = "none";
   score = 0;
   vies = 3;
   jouer();
@@ -62,29 +74,45 @@ function jouer() {
     boutonJouer.style.display = "none";
     boutonValider.style.display = "block";
     input.style.display = "block";
+    form.style.display = "flex";
     boutonReponse.style.display = "block";
-    volumeDiv.style.display = "block";
+    volumeTitre.style.display = "flex";
+    volumeDiv.style.display = "flex";
+    scoreDIV.style.display = "flex";
+    viesDIV.style.display = "flex";
   });
 }
 
 volMoins.addEventListener("click", () => {
+  iVolumeUp.style.color = "#e34069";
+  iVolumeUp.style.border = "1px solid #e34069";
   if (w <= 1) {
     w = 0;
+    iVolumeDown.style.color = "#383838";
+    iVolumeDown.style.border = "1px solid #383838";
     volumeText.innerHTML = `<i class="fas fa-volume-mute"></i>`;
   } else {
     w = w - 1;
+    iVolumeDown.style.color = "#e34069";
+    iVolumeDown.style.border = "1px solid #e34069";
     volumeText.innerHTML = `${w}0 %`;
   }
   audio.volume = `0.${w}`;
 });
 
 volPlus.addEventListener("click", () => {
+  iVolumeDown.style.color = "#e34069";
+  iVolumeDown.style.border = "1px solid #e34069";
   if (w >= 9) {
     w = 9;
     volumeText.innerHTML = `100 %`;
+    iVolumeUp.style.color = "#383838";
+    iVolumeUp.style.border = "1px solid #383838";
   } else {
     w = w + 1;
     volumeText.innerHTML = `${w}0 %`;
+    iVolumeUp.style.color = "#e34069";
+    iVolumeUp.style.border = "1px solid #e34069";
   }
   audio.volume = `0.${w}`;
 });
@@ -100,17 +128,21 @@ function getSong(x) {
 }
 
 function playSong(w) {
-  i = nombreAleatoire(1, 67);
+  i = 56;
   x = i - 1;
   getSong(x);
   y = songID;
   boutonReponse.style.display = "block";
   boutonRejouer.style.display = "block";
   boutonSuivant.style.display = "none";
+  scoreDIV.style.display = "flex";
   reponseDiv.innerHTML = "";
+  viesDIV.style.display = "flex";
   seconds.textContent = "0";
   startTimer();
   audioPlay(y, v);
+  lectureDIV.style.display = "flex";
+  lectureDIV.innerHTML = `Devine le titre !<br />`;
 }
 
 function audioPlay(y) {
@@ -180,15 +212,25 @@ function valider() {
       boutonReponse.style.display = "none";
       boutonValider.style.display = "none";
       seconds.style.display = "none";
+      timerContainer.style.display = "none";
       inputValue = "";
       input.value = "";
       chansonsTrouvees = chansonsTrouvees + 1;
       trouveesDiv.innerHTML = `Chansons trouvées : ${chansonsTrouvees}`;
       boutonSuivant.style.display = "block";
+      lectureDIV.innerHTML = `Prêt(e) pour le prochain titre ?`;
+      input.style.display = "none";
     } else if (scorePourcent < 75 && inputValue != "") {
       vies = vies - 1;
       viesDIV.innerHTML = `<h1>${vies}</h1>`;
       scoreChecker();
+      bonus.innerHTML = `<i class="fas fa-skull"></i>`;
+      bonus.style.display = "block";
+      bonus.classList.add("animation");
+
+      setTimeout(() => {
+        bonus.style.display = "none";
+      }, 1800);
     }
   });
 }
@@ -196,15 +238,20 @@ valider();
 
 function scoreChecker() {
   if (vies <= 0) {
-    viesDIV.innerHTML = `<h1>Perdu</h1>`;
+    viesDIV.innerHTML = `<h1 class="loose">Perdu</h1>`;
     boutonRejouer.style.display = "none";
     input.style.display = "none";
+    form.style.display = "none";
     boutonValider.style.display = "none";
     boutonNouvellePartie.style.display = "block";
     stopTimer();
     seconds.style.display = "none";
+    timerContainer.style.display = "none";
     volumeDiv.style.display = "none";
+    volumeTitre.style.display = "none";
     boutonSuivant.style.display = "none";
+    boutonReponse.style.display = "none";
+    reponseDiv.style.display = "flex";
   }
   if (vies == 3) {
     viesDIV.innerHTML = `<h1><i class="fas fa-heart"></i><i class="fas fa-heart"></i><i class="fas fa-heart"></i></h1>`;
@@ -221,7 +268,8 @@ function scoreChecker() {
 function rejouer() {
   boutonRejouer.addEventListener("click", () => {
     audioStop();
-    vies = vies - 1;
+    score = score - 1;
+    scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
     viesDIV.innerHTML = `<h1>${vies}</h1>`;
     scoreChecker();
     audio.volume = `0.${w}`;
@@ -237,15 +285,20 @@ boutonNouvellePartie.addEventListener("click", () => {
   playSong(w);
   boutonRejouer.style.display = "block";
   input.style.display = "block";
+  form.style.display = "flex";
   boutonValider.style.display = "block";
   boutonNouvellePartie.style.display = "none";
   inputValue = "";
   input.value = "";
-  scoreDIV.innerHTML = `<h1>${score}</h1>`;
+  scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
   viesDIV.innerHTML = `<h1><i class="fas fa-heart"></i><i class="fas fa-heart"></i><i class="fas fa-heart"></i></h1>`;
   seconds.style.display = "block";
+  timerContainer.style.display = "flex";
   chansonsTrouvees = 0;
-  volumeDiv.style.display = "block";
+  volumeDiv.style.display = "flex";
+  volumeTitre.style.display = "flex";
+  scoreDIV.style.display = "flex";
+  viesDIV.style.display = "flex";
 });
 
 function voirReponse(w) {
@@ -254,14 +307,18 @@ function voirReponse(w) {
   scoreChecker();
   stopTimer();
   seconds.style.display = "none";
+  timerContainer.style.display = "none";
 }
 
 boutonReponse.addEventListener("click", () => {
   audioStop();
-  reponseDiv.innerHTML = `${song}`;
+  reponseDiv.innerHTML = `Réponse : <span class="color">${song}</span>`;
   voirReponse(w);
   input.style.display = "none";
+  form.style.display = "none";
   boutonValider.style.display = "none";
+  lectureDIV.style.display = "flex";
+  lectureDIV.innerHTML = `<i class="fas fa-skull"></i>`;
 
   boutonReponse.style.display = "none";
   if (vies <= 0) {
@@ -289,6 +346,7 @@ function nextSong(w) {
   reponseDiv.innerHTML = "";
   boutonSuivant.style.display = "none";
   input.style.display = "block";
+  form.style.display = "flex";
   boutonValider.style.display = "block";
   boutonReponse.style.display = "block";
 }
@@ -298,24 +356,54 @@ boutonJouer.addEventListener("click", startTimer);
 function chronoChecker() {
   if (seconds.textContent > 0 && seconds.textContent <= 3) {
     score = score + 5;
-    scoreDIV.innerHTML = `<h1>${score}</h1>`;
+    scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
     console.log("+5");
+    bonus.innerHTML = `+5 Points`;
+    bonus.style.display = "block";
+    bonus.classList.add("animation");
+    setTimeout(() => {
+      bonus.style.display = "none";
+    }, 1800);
   } else if (seconds.textContent > 3 && seconds.textContent <= 8) {
     score = score + 3;
-    scoreDIV.innerHTML = `<h1>${score}</h1>`;
+    scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
     console.log("+3");
+    bonus.innerHTML = `+3 Points`;
+    bonus.style.display = "block";
+    bonus.classList.add("animation");
+    setTimeout(() => {
+      bonus.style.display = "none";
+    }, 1800);
   } else if (seconds.textContent > 8 && seconds.textContent <= 14) {
     score = score + 2;
-    scoreDIV.innerHTML = `<h1>${score}</h1>`;
+    scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
     console.log("+2");
+    bonus.innerHTML = `+2 Points`;
+    bonus.style.display = "block";
+    bonus.classList.add("animation");
+    setTimeout(() => {
+      bonus.style.display = "none";
+    }, 1800);
   } else if (seconds.textContent > 14 && seconds.textContent <= 59) {
     score = score + 1;
-    scoreDIV.innerHTML = `<h1>${score}</h1>`;
+    scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
     console.log("+1");
+    bonus.innerHTML = `+1 Point`;
+    bonus.style.display = "block";
+    bonus.classList.add("animation");
+    setTimeout(() => {
+      bonus.style.display = "none";
+    }, 1800);
   } else if (seconds.textContent > 59) {
     score = score + 0;
-    scoreDIV.innerHTML = `<h1>${score}</h1>`;
+    scoreDIV.innerHTML = `<h1><span>Score</span>${score}</h1>`;
     console.log("+0");
+    bonus.innerHTML = `+0 Point`;
+    bonus.style.display = "block";
+    bonus.classList.add("animation");
+    setTimeout(() => {
+      bonus.style.display = "none";
+    }, 1800);
   }
 }
 
@@ -327,6 +415,7 @@ function startTimer() {
   isRunning = true;
   interval = setInterval(incrementTimer, 1000);
   seconds.style.display = "block";
+  timerContainer.style.display = "flex";
 }
 
 function stopTimer() {
